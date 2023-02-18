78aaa0d7d46464bf99a424f21f31e5e0cccc2dc3
habib-N19 <habiburnabiarafat@gmail.com>
2023-02-18 17:58:58 +0600

    area calc table added

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,66 @@
+let serial = 0;
 // triangle card
+
 document.getElementById('triangle-calc-btn').addEventListener('click', function () {
     // console.log('first btn clicked')
+    serial += 1;
+    const name = 'Triangle';
     const inputFieldsValues = getInputValueByID('b-value-triangle', 'h-value-triangle');
     const areaTotal = 0.5 * inputFieldsValues;
-    const area = parseFloat(areaTotal).toFixed(2);
-    console.log(area);
+    const areaValue = parseFloat(areaTotal).toFixed(2);
+    displayResult(name, areaValue);
 
 
 })
 // rectangle card
 document.getElementById('rectangle-calc-btn').addEventListener('click', function () {
-    console.log('first btn clicked')
+    // console.log(' btn clicked')
+    serial += 1;
+    const name = 'Rectangle';
+    const inputFieldsValues = getInputValueByID('w-value-rectangle', 'l-value-rectangle');
+    const areaTotal = inputFieldsValues;
+    const area = parseFloat(areaTotal).toFixed(2);
+    displayResult(name, area);
 })
 // parallelogram card
 document.getElementById('parallelogram-calc-btn').addEventListener('click', function () {
-    console.log('first btn clicked')
+    // console.log('first btn clicked')
+    serial += 1;
+    const name = 'Parallelogram';
+    const inputFieldsValues = getInputValueByID('b-value-parallelogram', 'h-value-parallelogram');
+    const areaTotal = inputFieldsValues;
+    const area = parseFloat(areaTotal).toFixed(2);
+    displayResult(name, area);
 })
 // rhombus card
 document.getElementById('rhombus-calc-btn').addEventListener('click', function () {
-    console.log('first btn clicked')
+    // console.log('first btn clicked')
+    serial += 1;
+    const name = 'Rhombus';
+    const inputFieldsValues = getInputValueByID('d1-value-rhombus', 'd2-value-rhombus');
+    const areaTotal = 0.5 * inputFieldsValues;
+    const area = parseFloat(areaTotal).toFixed(2);
+    displayResult(name, area);
 })
 // pentagon card
 document.getElementById('pentagon-calc-btn').addEventListener('click', function () {
-    console.log('first btn clicked')
+    serial += 1;
+    const name = 'Pentagon';
+    const inputFieldsValues = getInputValueByID('p-value-pentagon', 'b-value-pentagon');
+    const areaTotal = 0.5 * inputFieldsValues;
+    const area = parseFloat(areaTotal).toFixed(2);
+    displayResult(name, area);
 })
 // ellipse card
 document.getElementById('ellipse-calc-btn').addEventListener('click', function () {
-    console.log('first btn clicked')
+    serial += 1;
+    const name = 'Ellipse';
+    const inputFieldsValues = getInputValueByID('a-value-ellipse', 'b-value-ellipse');
+    const areaTotal = 3.14 * inputFieldsValues;
+    const area = parseFloat(areaTotal).toFixed(2);
+    displayResult(name, area);
 })
+
+
+// result showing area
+
--- scripts/utilites.js
+++ scripts/utilites.js
@@ -1,10 +1,31 @@
 // function for all input validation and make the input string values to float as needed
 function getInputValueByID(input1, input2) {
-    const inputFieldValue1 = document.getElementById(input1).value;
-    // const inputFieldValue1 = inputField1.value;
-    const inputFieldValue2 = document.getElementById(input2).value;
-    // const= inputField2.value;
-    const inputFieldsValueTotal = inputFieldValue1 * inputFieldValue2;
+    const inputField1 = document.getElementById(input1);
+    const inputField1String = inputField1.value;
+    const inputValue1 = parseFloat(inputField1String);
+    const inputField2 = document.getElementById(input2);
+    const inputField2String = inputField2.value;
+    const inputValue2 = parseFloat(inputField2String);
+
+    if (isNaN(inputValue1) || inputValue1 <= 0 || isNaN(inputValue2) || inputValue2 <= 0) {
+        return alert('Please enter valid numbers')
+    }
+    const inputFieldsValueTotal = inputValue1 * inputValue2;
     return inputFieldsValueTotal;
+
+}
+
+// function for showing result on the side section
+function displayResult(name, result) {
+    const tableContainer = document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${serial} </td>
+    <td>${name} </td>
+    <td>${result} cm<sup>2</sup> </td>
+    <td><button class="convert-button" > m<sup>2</sup> </button/></td>
+    
+    `
+    tableContainer.appendChild(tr);
 
 }
